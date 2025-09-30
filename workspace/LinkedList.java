73ca5d051804fb9b6d1be64457c308a361806d55
rocketcat44 <blixinran@gmail.com>
2025-09-30 00:23:01 +0000

    asdjfkawkjdawdfjk

--- workspace/LinkedList.java
+++ workspace/LinkedList.java
@@ -1,52 +1,171 @@
 /*
-Problem:  Write a program that keeps and manipulates a linked list of
-	    String data. The data will be provided by the user one item at a time.
-      The user should be able to do the following operations:
-                     -add "String"
-                                adds an item to your list (maintaining alphabetical order)
-                     -remove "String"
-                                if the item exists removes the first instance of it
-                     -show
-                                should display all items in the linked list
-                     -clear
-                               should clear the list
-	Input:  commands listed above
-	Output:  the results to the screen of each menu
-	    choice, and error messages where appropriate.
+Bella Li 9/24/25
+
+
+This is a program that keeps and manipulates a linked list of
+     String data. The data will be provided by the user one item at a time.
+     The user should be able to do the following operations:
+                    -add "String"
+                               adds an item to your list (maintaining alphabetical order)
+                    -remove "String"
+                               if the item exists removes the first instance of it
+                    -show
+                               should display all items in the linked list
+                    -clear
+                              should clear the list
+ Input:  commands listed above
+ Output:  the results to the screen of each menu
+     choice, and error messages where appropriate.
 */
 public class LinkedList{
 
-  //instance varialbes go here (think about what you need to keep track of!)
 
-  //constructors go here
+ //instance varialbes go here (think about what you need to keep track of!)
+ public ListNode head;
+ public int size;
 
 
-  //precondition: the list has been initialized
-  //postcondition: the ListNode containing the appropriate value has been added and returned
-  public ListNode addAValue(String line)
-  {
-    return null;
-  }
+ //constructors go here
+ public LinkedList()
+ {
+   head = null;
+   size = 0;
+ }
 
-  //precondition: the list has been initialized
-  //postcondition: the ListNode containing the appropriate value has been deleted and returned.
-  //if the value is not in the list returns null
-  public ListNode deleteAValue(String line)
-  {
-    return null;
-  }
 
-  //precondition: the list has been initialized
-  //postconditions: returns a string containing all values appended together with spaces between.
-  public String showValues()
-  {
-    return null;
-  }
+ //precondition: the list has been initialized
+ //postcondition: reverses the ENTIRE linked list (the tail will be the new head and the head the new tail)
+ public ListNode reverse(){
+   ListNode temp = head;
+   ListNode tempNext = null;
+   ListNode tempPrev = null;
 
-  //precondition: the list has been initialized
-  //postconditions: clears the list.
-  public void clear()
-  {
-  
-  }
+
+   head = tempNext;
+
+
+   while (temp != null) {
+
+
+           tempNext = temp.getNext();
+           temp.setNext(tempPrev);
+           tempPrev = temp;
+           // Go to next node
+           temp = tempNext;
+       }
+
+
+       head = tempPrev;
+       return tempPrev; //placeholder
+ }
+
+
+ //precondition: the list has been initialized, n is not larger than the number of elements in the list, list will always contain at least one element
+ //postcondition: Reverses each "chunk" of n nodes (if there aren't enough nodes at the end to fit into a chunk, they are left without being reversed)
+ public ListNode nReverse(LinkedList ogList, int n){
+   ListNode temp = head;
+   ListNode tempNext = null;
+   ListNode tempPrev = null;
+   ListNode tempEnd = null;
+   ListNode prevChunkEnd = null;
+
+
+   while (temp != null) {
+     ListNode chunkStart = temp;
+     tempEnd = temp;
+     for (int i = 0; i < n && tempEnd != null; i++) { // finds the end of the temp chunk
+       tempEnd = tempEnd.getNext();
+     }
+
+
+     int count = 0;
+     tempPrev = null;
+     ListNode node = temp;
+     while (node != tempEnd) { // reverses the chunk
+       tempNext = node.getNext();
+       node.setNext(tempPrev);
+       tempPrev = node;
+       node = tempNext;
+       count++;
+     }
+
+
+     if (prevChunkEnd == null) {
+       head = tempPrev; // set new head for the first chunk
+     } else {
+       prevChunkEnd.setNext(tempPrev);
+     }
+     chunkStart.setNext(tempEnd);
+     prevChunkEnd = chunkStart;
+     temp = tempEnd;
+   }
+   return head;
+ }
+
+
+ //precondition: the list has been initialized
+ //postcondition: the ListNode containing the appropriate value has been added and returned
+ public ListNode addAValue(String line) {
+   ListNode newNode = new ListNode(line, null);
+   if (head == null || head.getValue().compareTo(line) > 0) {
+       newNode.setNext(head);
+       head = newNode;
+       return newNode;
+   }
+   ListNode current = head;
+   while (current.getNext() != null && current.getNext().getValue().compareTo(line) < 0) {
+       current = current.getNext();
+   }
+   newNode.setNext(current.getNext());
+   current.setNext(newNode);
+   return newNode;
+ }
+
+
+ //precondition: the list has been initialized
+ //postcondition: the ListNode containing the appropriate value has been deleted and returned.
+ //if the value is not in the list returns null
+ public ListNode deleteAValue(String line) {
+ if (head == null) {
+   System.out.println(line + " not found");
+   return null;
+ }
+ if (head.getValue().equals(line)) {
+   ListNode removed = head;
+   head = head.getNext();
+   return removed;
+ }
+ ListNode current = head;
+ while (current.getNext() != null && !current.getNext().getValue().equals(line)) {
+   current = current.getNext();
+ }
+ if (current.getNext() == null) {
+   System.out.println(line + " not found");
+   return null;
+ }
+ ListNode removed = current.getNext();
+ current.setNext(removed.getNext());
+ return removed;
+ }
+
+
+ //precondition: the list has been initialized
+ //postconditions: returns a string containing all values appended together with spaces between.
+ public String showValues() {
+   StringBuilder sb = new StringBuilder();
+   ListNode current = head;
+   while (current != null) {
+       sb.append(current.getValue());
+       if (current.getNext() != null) sb.append(" ");
+       current = current.getNext();
+   }
+   return sb.toString();
+ }
+
+
+ //precondition: the list has been initialized
+ //postconditions: clears the list.
+ public void clear() {
+   head = null;
+ }
 }
--- workspace/Main.java
+++ workspace/Main.java
@@ -1,43 +1,58 @@
-//This file contains an example of how to read user input. Use this in conjunction with your linked list class to test your program.
+//Bella Li 9/24/25
+//This file contains an example of how to read user input. Used in conjunction with the linked list class to test the program.
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 public class Main{
-    public static void main(String[] args)
-        throws IOException
-    {
-        // Enter data using BufferReader
-        BufferedReader reader = new BufferedReader(
-            new InputStreamReader(System.in));
+   public static void main(String[] args)
+       throws IOException
+   {
+       // Enter data using BufferReader
+       BufferedReader reader = new BufferedReader(
+           new InputStreamReader(System.in));
 
-        // Reading data using readLine
-        LinkedList list = new LinkedList();
-        String input= reader.readLine();
-        while(!input.equals("exit"))
-        {
-          if(input.length()>3 && input.substring(0, 3).equals("add"))
-          {
-            System.out.println(list.addAValue(input.substring(4)).getValue());
-          }
-          else if(input.equals("show"))
-          {
-            System.out.println(list.showValues());
-          }
-          else if(input.length() > 7 && input.substring(0, 6).equals("remove")){
-              System.out.println("removing "+input.substring(7));
-              System.out.println(list.deleteAValue(input.substring(7)).getValue());
-          }
-          else if(input.equals("clear"))
-          {
-            list.clear();
-          }
-          else if(!input.equals("exit")){
-              System.out.println("I don't know how to "+input);
-          }
-          input= reader.readLine();
-        }
- 
-        // Printing the read line
-        System.out.println("thanks for playing!");
-    }
+
+       // Reading data using readLine
+       LinkedList list = new LinkedList();
+       String input= reader.readLine();
+       while(!input.equals("exit"))
+       {
+         if(input.length()>3 && input.substring(0, 3).equals("add"))
+         {
+           System.out.println(list.addAValue(input.substring(4)).getValue());
+         }
+         else if(input.equals("show"))
+         {
+           System.out.println(list.showValues());
+         }
+         else if(input.length() > 7 && input.substring(0, 6).equals("remove")){
+             System.out.println("removing "+input.substring(7));
+             ListNode lalala = list.deleteAValue(input.substring(7));
+             if (lalala != null){
+               System.out.println(lalala.getValue());
+             }
+            
+         }
+         else if(input.equals("clear"))
+         {
+           list.clear();
+           System.out.println("list cleared");
+         }else if (input.equals("reverse")){
+           list.reverse();
+           System.out.println("list reversed: " + list.showValues());
+
+
+         }else if (input.contains("nreverse")){ // example: "nreverse 3" -- don't foget the space
+           list.nReverse(list, Integer.parseInt(input.substring(9)));
+           System.out.println("list nreversed: " + list.showValues());
+
+
+         }else if(!input.equals("exit")){
+             System.out.println("I don't know how to "+input);
+         }
+         input= reader.readLine();
+       }
+       // Printing the read line
+       System.out.println("thanks for playing!");
+   }
 }
